65800f56c4f335b5917bf0564a994066ed63f7c4
kroonky <francoofarace@gmail.com>
2025-06-07 21:19:41 -0300

    Update funciones.h

--- include/funciones.h
+++ include/funciones.h
@@ -6,8 +6,18 @@ void altaTransferencia();
 void bajaTransferencia(int id);
 void modificarTransferencia(int id);
 void listarTransferencias();
-
-
+void altaJugador();
+void bajaJugador(int id);
+void modificarJugador(int id);
+void listarJugador();
+void altaClub();
+void bajaClub(int id);
+void modificarClub(int id);
+void listarClub();
+void altaEstadisticas();
+void bajaEstadisticas(int id);
+void modificarEstadisticas(int id);
+void listarEstadisticas();
 
 
 
